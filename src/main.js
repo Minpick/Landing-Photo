9c80e62a02731ae4c58fcb74f804c8b8e235fa96
Minpick <152726605+Minpick@users.noreply.github.com>
2025-01-20 17:12:10 +0400

    few more sections

---
+++ src/main.js
@@ -0,0 +1,75 @@
+document.addEventListener('scroll', () => {
+  // Получаем текущую прокрутку
+  const scrollPosition = window.scrollY;
+  const header = document.getElementById('header')
+  const headerContainer = document.getElementById('header-container')
+  // Проверяем, если прокрутка больше 100vh
+  if (header) {
+    if (scrollPosition > window.innerHeight - 80) {
+      header.style.backgroundColor = 'var(--black-transparent)';
+      headerContainer.style.borderBottomColor = 'transparent' // Новый цвет
+    } else {
+      header.style.backgroundColor = 'transparent'; // Начальный цвет
+      headerContainer.style.borderBottomColor = 'var(--black-70)'; // Начальный цвет
+    }
+  }
+});
+
+
+// Находим элементы
+const modal = document.getElementById('videoModal');
+const closeBtn = document.getElementById('closeBtn');
+const modalVideo = document.getElementById('modalVideo');
+
+// Находим все кнопки, открывающие модальное окно, по классу
+const openModalButtons = document.querySelectorAll('.open-modal');
+
+// Функция для открытия модалки и подстановки нужного видео
+function openModal(videoId) {
+  // Подставляем ID в ссылку для YouTube
+  // '?autoplay=1' запустит видео автоматически 
+  modalVideo.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+  modal.style.display = 'flex'; // показываем модал (flex для выравнивания)
+}
+
+// Функция для закрытия модалки
+function closeModal() {
+  modal.style.display = 'none';
+  modalVideo.src = ''; // сбрасываем src, чтобы видео остановилось
+}
+
+// Вешаем обработчик на все кнопки
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const videoId = button.getAttribute('data-video-id');
+    openModal(videoId);
+  });
+});
+
+// Закрытие по клику на &times;
+closeBtn.addEventListener('click', closeModal);
+
+// (Опционально) Закрытие по клику вне области контента
+window.addEventListener('click', event => {
+  if (event.target === modal) {
+    closeModal();
+  }
+});
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+
